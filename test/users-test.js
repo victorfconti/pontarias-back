4230ff27f575cd9fd7f144c1eb8852ba674b17b2
= <victorfconti@gmail.com>
2019-12-27 00:37:17 -0300

    Configuration of chaiHttp

--- test/users-test.js
+++ test/users-test.js
@@ -1,7 +1,13 @@
 const assert = require('assert');
+const chai = require('chai');
+const app = require('../app');
+let chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
 
 describe('User', () => {
     it('true', function () {
+        chai.request(app);
         assert.equal(3, 3);
     });
 });
