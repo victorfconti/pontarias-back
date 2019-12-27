8dddce3463140d74b2999baf7c78a9d802ef3c09
= <victorfconti@gmail.com>
2019-12-27 00:06:59 -0300

    Envroiment variable set for test

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'test';
+const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
