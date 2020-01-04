f1d0560daacfa352df3cec7b7d77e308c7d6629a
= <victorfconti@gmail.com>
2020-01-03 22:49:18 -0300

    Country endpoint finished

--- controllers/country.js
+++ controllers/country.js
@@ -1,13 +1,14 @@
 const userModel = require('../models/index').Country;
 const logger = require('../config/logger');
 const { validationResult } = require('express-validator');
+const sequelize = require('sequelize');
 
 module.exports = {
 
     get(req, res){
-        userModel.findAll().then((countries)=>{
+        userModel.findAll().then(countries =>{
             return res.json(countries)
-        }).catch((error)=>{
+        }).catch(error =>{
             logger.error(error);
             return res.json(error).status(500);
         });
@@ -18,7 +19,7 @@ module.exports = {
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        userModel.findByPk(req.params.id).then((country)=>{
+        userModel.findByPk(req.params.id).then(country =>{
             return res.json(country);
         }).catch(error=>{
             logger.error(error);
@@ -31,7 +32,8 @@ module.exports = {
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        res.json({hello: 'world'});
+        return this.findOneUser(res,
+            {where:{country: sequelize.where(sequelize.fn('LOWER', sequelize.col('country')), req.params.name.toLowerCase())}});
     },
 
     getWithAlpha2(req, res){
@@ -39,7 +41,16 @@ module.exports = {
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        res.json({hello: 'world'});
-    }
+        return this.findOneUser(res,
+            {where: { alpha2: sequelize.where(sequelize.fn('LOWER', sequelize.col('alpha2')), req.params.alpha2.toLowerCase())}});
+    },
 
+    findOneUser(res, customJsonQuery){
+        userModel.findOne(customJsonQuery).then(country => {
+            return res.json(country == null?{}:country);
+        }).catch(error => {
+            logger.error(error);
+            return res.status(500).json({errors: error['errors']});
+        });
+    }
 };
