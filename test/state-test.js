130950f878825419dacb8c7861d4ee95a756384b
= <victorfconti@gmail.com>
2020-01-23 21:52:52 -0300

    Code coverage for state completed

--- test/state-test.js
+++ test/state-test.js
@@ -14,12 +14,12 @@ describe('State', ()=>{
            return CountryModel.create({name: 'Brazil',alpha2: 'BR', alpha3: 'BRZ', un: '004'}).
            then(c => {
                const id = c['id'];
-               return StateModel.create({name: 'São Paulo', abbreviation: 'SP', CountryId: id});
+               return StateModel.create({name: 'Pernambuco', abbreviation: 'PE', CountryId: id});
            });
         }
     });
 
-    it ('Get all',() =>{
+    it ('Get by country',() =>{
         CountryModel.findOne({where: {name: 'Brazil'}}).then(c=>{
             const countryId = c['id'];
             chai.request(app).get('/states/country/'+countryId).end((err, res)=>{
@@ -45,108 +45,66 @@ describe('State', ()=>{
             chai.AssertionError;
         });
     });
+    it('Get by name',() =>{
+        chai.request(app).get('/states/name/Pernambuco').end((err, res)=>{
+            console.log(res);
+            chai.expect(res.status).is.equal(200);
+            chai.expect(res.body.name).is.equal('Pernambuco');
+        });
+    });
+    it('Get by country constraint error',() =>{
+        const mResponse = new mockResponse();
+        const state = new State({
+            findOne: ()=>{
+                return new Promise(((resolve, reject) => reject()));
+            }
+        });
+        state.getByName({params: {name: 'a'}}, mResponse).then(
+            ()=>{
+                chai.expect(mResponse.statusCode).is.eq(500);
+                chai.expect(mResponse.jsonObject).is.empty;
+            }
+        ).catch(()=>{
+            chai.AssertionError;
+        });
 
-    // it('Get by id',() =>{
-    //     chai.request(app).get('/countries').end((err, res)=>{
-    //         chai.request(app).get('/countries/' + res.body[0].id).end((errInner, resInner)=>{
-    //             chai.expect(err).is.null;
-    //             chai.expect(errInner).is.null;
-    //             chai.expect(resInner.status).is.equal(200);
-    //             chai.expect(resInner.body.id).is.equal(res.body[0].id);
-    //         });
-    //     });
-    // });
-    // it('Get by invalid id with error',() =>{
-    //     chai.request(app).get('/countries/null').end((err, res)=>{
-    //         chai.expect(err).is.null;
-    //         chai.expect(res.status).is.equal(404);
-    //         chai.expect(res.body).is.empty;
-    //     });
-    // });
-    // it('Get by id constraint error',() =>{
-    //     const mResponse = new mockResponse();
-    //     const country = new Country({
-    //         findByPk: ()=>{
-    //             return new Promise(((resolve, reject) => reject()));
-    //         }
-    //     });
-    //     country.getWithId({params: {id: 1}}, mResponse).then(
-    //         ()=>{
-    //             chai.expect(mResponse.statusCode).is.eq(500);
-    //             chai.expect(mResponse.jsonObject).is.empty;
-    //         }
-    //     ).catch(()=>{
-    //         chai.AssertionError;
-    //     });
-    //
-    // });
-    // it('Get by country',() =>{
-    //     chai.request(app).get('/countries').end((err, res)=>{
-    //         chai.request(app).get('/countries/name/' + res.body[0].name).end((errInner, resInner)=>{
-    //             console.log(resInner);
-    //             chai.expect(err).is.null;
-    //             chai.expect(errInner).is.null;
-    //             chai.expect(resInner.status).is.equal(200);
-    //             chai.expect(resInner.body.name).is.equal(res.body[0].name);
-    //         });
-    //     });
-    // });
-    // it('Get by country constraint error',() =>{
-    //     const mResponse = new mockResponse();
-    //     const country = new Country({
-    //         findOne: ()=>{
-    //             return new Promise(((resolve, reject) => reject()));
-    //         }
-    //     });
-    //     country.getWithName({params: {name: 'a'}}, mResponse).then(
-    //         ()=>{
-    //             chai.expect(mResponse.statusCode).is.eq(500);
-    //             chai.expect(mResponse.jsonObject).is.empty;
-    //         }
-    //     ).catch(()=>{
-    //         chai.AssertionError;
-    //     });
-    //
-    // });
-    // it('Get by invalid country with error',() =>{
-    //     chai.request(app).get('/countries/name/9999').end((err, res)=>{
-    //         chai.expect(err).is.null;
-    //         chai.expect(res.status).is.equal(404);
-    //         chai.expect(res.body).is.empty;
-    //     });
-    // });
-    // it('Get by alpha2',() =>{
-    //     chai.request(app).get('/countries').end((err, res)=>{
-    //         chai.request(app).get('/countries/alpha2/' + res.body[0].alpha2).end((errInner, resInner)=>{
-    //             chai.expect(err).is.null;
-    //             chai.expect(errInner).is.null;
-    //             chai.expect(resInner.status).is.equal(200);
-    //             chai.expect(resInner.body.alpha2).is.equal(res.body[0].alpha2);
-    //         });
-    //     });
-    // });
-    // it('Get by alpha2 constraint error',() =>{
-    //     const mResponse = new mockResponse();
-    //     const country = new Country({
-    //         findOne: ()=>{
-    //             return new Promise(((resolve, reject) => reject()));
-    //         }
-    //     });
-    //     country.getWithAlpha2({params: {alpha2: 'a'}}, mResponse).then(
-    //         ()=>{
-    //             chai.expect(mResponse.statusCode).is.eq(500);
-    //             chai.expect(mResponse.jsonObject).is.empty;
-    //         }
-    //     ).catch(()=>{
-    //         chai.AssertionError;
-    //     });
-    //
-    // });
-    // it('Get by invalid alpha2 with error',() => {
-    //     chai.request(app).get('/countries/alpha2/999').end((err, res) => {
-    //         chai.expect(err).is.null;
-    //         chai.expect(res.status).is.equal(404);
-    //         chai.expect(res.body).is.empty;
-    //     });
-    // });
+    });
+    it('Get by invalid country with error',() =>{
+        chai.request(app).get('/states/name/9999').end((err, res)=>{
+            chai.expect(err).is.null;
+            chai.expect(res.status).is.equal(404);
+            chai.expect(res.body).is.empty;
+        });
+    });
+    it('Get by ',() =>{
+        chai.request(app).get('/states/abbreviation/PE').end((err, res)=>{
+            chai.expect(err).is.null;
+            chai.expect(res.status).is.equal(200);
+            chai.expect(res.body.abbreviation).is.equal('PE');
+        });
+    });
+    it('Get by alpha2 abbreviation error',() =>{
+        const mResponse = new mockResponse();
+        const state = new State({
+            findOne: ()=>{
+                return new Promise(((resolve, reject) => reject()));
+            }
+        });
+        state.getByAbbreviation({params: {abbreviation: 'a'}}, mResponse).then(
+            ()=>{
+                chai.expect(mResponse.statusCode).is.eq(500);
+                chai.expect(mResponse.jsonObject).is.empty;
+            }
+        ).catch(()=>{
+            chai.AssertionError;
+        });
+
+    });
+    it('Get by invalid abbreviation with error',() => {
+        chai.request(app).get('/states/abbreviation/999').end((err, res) => {
+            chai.expect(err).is.null;
+            chai.expect(res.status).is.equal(404);
+            chai.expect(res.body).is.empty;
+        });
+    });
 });
