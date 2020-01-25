3276ae6325011cf6e2bd8586e8f2b67f832be1ca
= <victorfconti@gmail.com>
2020-01-25 12:31:04 -0300

    Creation of address

---
+++ models/address.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = (sequelize, datatype)=>{
+    return sequelize.define('Address', {
+        street: {type: datatype.STRING, allowNull: false, trim: true},
+        number: {type: datatype.STRING, allowNull: false, trim: true},
+        complement: {type: datatype.STRING, allowNull: true, trim: true},
+        zip_code: {type: datatype.STRING, allowNull: false, trim: true},
+        observation: {type: datatype.STRING, allowNull: false, trim: true},
+        coordinates: {type: datatype.STRING, allowNull: true, trim: true}
+    });
+};
